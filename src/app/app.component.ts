d5ee47296fbd178cac317f12024f18dc79bd49a2
giorgioGTelian <giorgio.programmer@gmail.com>
2023-02-17 15:28:53 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
 
 
 /*
+tutorial flollwing:
 Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
 can be found in the LICENSE file at https://angular.io/license
